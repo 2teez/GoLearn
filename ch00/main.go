f43d92357590cb773e3d1e87d693a73182a12699
timothy adigun <adigun.timothy@gmail.com>
2025-02-18 02:20:56 +0000

    added function to the main function

--- ch00/main.go
+++ ch00/main.go
@@ -6,4 +6,16 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
+	for i := 0; i < 5; i++ {
+		PrintGreet()
+		PrintCounter(i)
+	}
+}
+
+func PrintCounter(number int) {
+	fmt.Println(number)
+}
+
+func PrintGreet() {
+	fmt.Println("Hello Go!")
 }
