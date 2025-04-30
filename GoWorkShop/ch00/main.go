5f639584825614084ea90ff0fb082d285a319c16
timothy adigun <adigun.timothy@gmail.com>
2025-04-30 08:40:42 +0100

    call the new functions

--- GoWorkShop/ch00/main.go
+++ GoWorkShop/ch00/main.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
 	random()
+	// make patient
+	bob := makePatient("bob", "smith", 34, false)
+	fmt.Println(bob)
+	bob2 := makePatientDefault()
+	fmt.Println(bob2)
 }
