05b8a799214ee64fa59c3e5c83779a15769a9224
timothy adigun <adigun.timothy@gmail.com>
2025-04-30 10:27:55 +0100

    added the swapper function from main file

--- GoWorkShop/ch01/goworkshop_ch01.go
+++ GoWorkShop/ch01/goworkshop_ch01.go
@@ -1,6 +1,36 @@
 package main
 
-import "strconv"
+import (
+	"math/rand"
+	"strconv"
+)
+
+type (
+	ptFuncTy func(*int, *int)
+	intSlice []int
+)
+
+func swapper(a, b *int) {
+	if *a > *b {
+		*b, *a = *a, *b
+	}
+}
+
+func sortNumbers(f ptFuncTy, nums intSlice) {
+	for range len(nums) - 1 {
+		for i := range len(nums) - 1 {
+			f(&nums[i], &nums[i+1])
+		}
+	}
+}
+
+func generateNumbers(times, ranged int) intSlice {
+	values := make(intSlice, 0, times)
+	for range times {
+		values = append(values, rand.Intn(ranged)+1)
+	}
+	return values
+}
 
 func fizzBuzz(number int) string {
 	switch {
