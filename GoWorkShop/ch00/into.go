c96f76e3b6d046bcc1a272d946d3ca7ce6aba78c
timothy adigun <adigun.timothy@gmail.com>
2025-04-30 08:40:32 +0100

    added function and type

--- GoWorkShop/ch00/into.go
+++ GoWorkShop/ch00/into.go
@@ -6,6 +6,37 @@ import (
 	"strings"
 )
 
+type String string
+
+type Patient struct {
+	FirstName     string
+	LastName      string
+	Age           int
+	PeanutAllergy bool
+}
+
+func (p *Patient) String() string {
+	return fmt.Sprintf("Patient {\n\tName: %s, %s\n\tAge: %d\n\tPeanut Allergy: %v\n}",
+		p.LastName, p.FirstName, p.Age, p.PeanutAllergy)
+}
+
+func makePatient(firstname, lastname string, age int, peanutAllergy bool) *Patient {
+	return &Patient{
+		String(firstname).toLocalTitle(),
+		String(lastname).toLocalTitle(), age, peanutAllergy,
+	}
+}
+
+func makePatientDefault() *Patient {
+	return &Patient{}
+}
+
+func (s String) toLocalTitle() string {
+	ls := string(s)
+	ls = strings.ToUpper(ls)
+	return (ls[:1] + strings.ToLower(ls[1:]))
+}
+
 func random() {
 	for range 10 {
 		r := rand.Intn(10) + 1
