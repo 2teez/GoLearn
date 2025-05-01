2e0528cef197bc16d69d74c9a1fd4ae732d94c83
timothy adigun <adigun.timothy@gmail.com>
2025-05-01 11:28:30 +0100

    used an alias for shape module

--- GoWorkShop/ch03/main.go
+++ GoWorkShop/ch03/main.go
@@ -4,18 +4,18 @@ import (
 	"fmt"
 
 	"github.com/practice/msg/msg"
-	"github.com/practice/msg/shape"
+	s "github.com/practice/msg/shape"
 )
 
 func main() {
 	fmt.Print(msg.Greeting("Hello...\n"))
-	shapes := []shape.Shape{shape.Triangle{Base: 6, Height: 4.5},
-		shape.Square{Length: 4.25}, shape.Rectangle{Height: 3.12, Width: 4.21}}
+	shapes := []s.Shape{s.Triangle{Base: 6, Height: 4.5},
+		s.Square{Length: 4.25}, s.Rectangle{Height: 3.12, Width: 4.21}}
 	printShapesDetails(shapes...)
 
 }
 
-func printShapesDetails(shapes ...shape.Shape) {
+func printShapesDetails(shapes ...s.Shape) {
 	for _, sh := range shapes {
 		fmt.Printf("Name: %s, Area: %.2f\n", sh.Name(), sh.Area())
 	}
