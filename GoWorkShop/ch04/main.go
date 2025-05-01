fbdd708a3e0343f5a3e488329b8d5b9ef391de88
timothy adigun <adigun.timothy@gmail.com>
2025-05-01 17:22:22 +0100

    using function to unmarshal(decoding) and marshalling(encoding) file in golang

--- GoWorkShop/ch04/main.go
+++ GoWorkShop/ch04/main.go
@@ -1,9 +1,45 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Start Here...")
+	var v greeting
+	data := []byte(`
+		{
+		"message": "Hello, Gophers!"
+		}
+		`)
+	unmarshalJson(data, &v)
+	fmt.Printf("%s, %+v\n", v.Message, v)
+	var student student
+	stdData := []byte(`{
+		"id": 123,
+		"lname": "Smith",
+		"minitial": null,
+		"fname": "John",
+		"enrolled": true,
+		"classes": [{
+		"coursename": "Intro to Golang",
+		"coursenum": 101,
+		"coursehours": 4
+		},
+		{
+		"coursename": "English Lit",
+		"coursenum": 101,
+		"coursehours": 3
+		},
+		{
+		"coursename": "World History",
+		"coursenum": 101,
+		"coursehours": 3
+		}
+		]
+		}
+`)
+	unmarshalJson(stdData, &student)
+	fmt.Printf("%+v", student)
+
+	//
+	marshalJson(student)
+	marshalJson(v)
 }
