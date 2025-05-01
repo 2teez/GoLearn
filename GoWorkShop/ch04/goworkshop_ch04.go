812e7ee7751d6f6280a68aa7a639c5e13628b929
timothy adigun <adigun.timothy@gmail.com>
2025-05-01 17:21:36 +0100

    using json in golang

--- GoWorkShop/ch04/goworkshop_ch04.go
+++ GoWorkShop/ch04/goworkshop_ch04.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type greeting struct {
+	Message string
+}
+
+type student struct {
+	StudentId     int      `json:"id"`
+	LastName      string   `json:"lname"`
+	MiddleInitial string   `json:"-"`
+	FirstName     string   `json:"fname"`
+	IsEnrolled    bool     `json:"enrolled,omitempty"`
+	Courses       []course `json:"classes"`
+}
+
+type course struct {
+	Name   string `json:"coursename"`
+	Number int    `json:"coursenumber"`
+	Hours  int    `json:"coursehours"`
+}
+
+func unmarshalJson(data []byte, v any) {
+	err := json.Unmarshal(data, &v)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func marshalJson(s any) {
+	jsData, err := json.MarshalIndent(s, "", "	")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("\n", string(jsData))
+}
