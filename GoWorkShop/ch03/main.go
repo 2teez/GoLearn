06dd5692637041ce52c06ccc7a15c2f9fd29774e
timothy adigun <adigun.timothy@gmail.com>
2025-05-01 11:24:57 +0100

    added module shape to main

--- GoWorkShop/ch03/main.go
+++ GoWorkShop/ch03/main.go
@@ -4,8 +4,19 @@ import (
 	"fmt"
 
 	"github.com/practice/msg/msg"
+	"github.com/practice/msg/shape"
 )
 
 func main() {
-	fmt.Print(msg.Greeting("Hello..."))
+	fmt.Print(msg.Greeting("Hello...\n"))
+	shapes := []shape.Shape{shape.Triangle{Base: 6, Height: 4.5},
+		shape.Square{Length: 4.25}, shape.Rectangle{Height: 3.12, Width: 4.21}}
+	printShapesDetails(shapes...)
+
+}
+
+func printShapesDetails(shapes ...shape.Shape) {
+	for _, sh := range shapes {
+		fmt.Printf("Name: %s, Area: %.2f\n", sh.Name(), sh.Area())
+	}
 }
