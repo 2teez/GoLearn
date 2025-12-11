aef08402c58d981eea64301514dabc082d4d7633
timothy adigun <adigun.timothy@gmail.com>
2025-12-11 11:09:23 +0000

    include getfloat function

--- bkz/Learning_go/junkz/pkg/src/readers/readers.go
+++ bkz/Learning_go/junkz/pkg/src/readers/readers.go
@@ -2,7 +2,10 @@ package readers
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func ReadLine() (string, error) {
@@ -12,4 +15,19 @@ func ReadLine() (string, error) {
 		return string([]byte{}), err
 	}
 	return string(n), nil
+}
+
+func GetFloat(msg string) (float64, error) {
+	fmt.Printf("%s", msg)
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	input = strings.TrimSpace(input)
+	value, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return value, nil
 }
