2ddeff586acda096d8aa6df38054632638974007
timothy adigun <adigun.timothy@gmail.com>
2025-12-17 00:40:12 +0000

    goutrine practice more

--- practiced/goroutine/clock/main.go
+++ practiced/goroutine/clock/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
 )
 
 func main() {
-	fmt.Println("Start Here...")
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().GoString())
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
