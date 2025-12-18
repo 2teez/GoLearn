337a57d4d533017f5751cc36a64d67f43528390b
timothy adigun <adigun.timothy@gmail.com>
2025-12-18 11:16:21 +0000

    main function in mutex in golang practice

--- practiced/goroutine/junkz/x3/main.go
+++ practiced/goroutine/junkz/x3/main.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+const letters = "abcdefghijklmnopqrstuvwxyz"
+
 func main() {
 	money := 100
 	mu := sync.Mutex{}
@@ -19,6 +21,20 @@ func main() {
 	}()
 	time.Sleep(1 * time.Second)
 	mu.Lock()
-	defer mu.Unlock()
 	fmt.Println("Money in the account: ", money)
+	mu.Unlock()
+	//
+	// using letter count frequency table
+	mu2 := sync.Mutex{}
+	freq := make([]int, 26)
+	for i := 1000; i < 1030; i++ {
+		url := fmt.Sprintf("https://www.rfc-editor.org/rfc/rfc%d.txt", i)
+		go countLetters(url, freq, &mu2)
+	}
+	time.Sleep(1 * time.Second)
+	mu2.Lock()
+	defer mu2.Unlock()
+	for ind := range len(letters) {
+		fmt.Printf("%c-%d ", letters[ind], freq[ind])
+	}
 }
