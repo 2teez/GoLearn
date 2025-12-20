dafd649ddb089f9071cd3554889ef48b0958cf96
timothy adigun <adigun.timothy@gmail.com>
2025-12-20 08:32:49 +0000

    organize the  file in correct directory

---
+++ LetGo/snippetbox/cmd/web/handlers.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	//w.Write([]byte("Display a specific Snippet"))
+	fmt.Fprintf(w, "Display a specific Snippet with an ID %d", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		//w.WriteHeader(405)
+		//w.Write(([]byte("Method Not allowed")))
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new Snippet"))
+}
