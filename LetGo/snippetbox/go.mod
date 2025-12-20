fab3c8eda159d02fe87c8f6d5ee144ddd36c8009
timothy adigun <adigun.timothy@gmail.com>
2025-12-20 07:44:31 +0000

    web application using golang

---
+++ LetGo/snippetbox/go.mod
@@ -0,0 +1,3 @@
+module snippetbox.timothyadigun.net
+
+go 1.25.4
---
+++ LetGo/snippetbox/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("Starting Server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific Snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		//w.WriteHeader(405)
+		//w.Write(([]byte("Method Not allowed")))
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+	w.Write([]byte("Create a new Snippet"))
+}
