94fa503d4a67a4b2ca82546648c752a5c51c926a
timothy adigun <adigun.timothy@gmail.com>
2025-12-20 14:21:18 +0000

    included an error function

--- practiced/junkz/webapp/cmd/web/handlers.go
+++ practiced/junkz/webapp/cmd/web/handlers.go
@@ -1,10 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
+func check(err error) {
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func write(w http.ResponseWriter, message string) {
+	_, err := w.Write([]byte(message))
+	check(err)
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Web...")
+	//fmt.Fprintf(w, "Hello, Web...")
+	write(w, "Hello, Web..")
 }
