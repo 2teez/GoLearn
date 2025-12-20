6bdb2deb5b080f773edb3b392dd14b8aa622d620
timothy adigun <adigun.timothy@gmail.com>
2025-12-20 10:09:57 +0000

    configured the home page and starting server

---
+++ practiced/junkz/webapp/cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	server := http.NewServeMux()
+	server.HandleFunc("/", home)
+
+	log.Println("Starting server on :8118")
+	err := http.ListenAndServe(":8118", server)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
