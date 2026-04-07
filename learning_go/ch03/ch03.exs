cdadb0d9b078ab374d087019a717f32d746fa7e4
timothy adigun <adigun.timothy@gmail.com>
2026-04-07 17:39:56 +0100

    moved the test module to a different file

--- learning_go/ch03/ch03.exs
+++ learning_go/ch03/ch03.exs
@@ -30,24 +30,3 @@ third_employee = EmployeeFunc.new("Jane", "Mary", 3)
 IO.inspect(first_employee)
 IO.inspect(second_employee)
 IO.inspect(third_employee)
-
-ExUnit.start()
-# Code.require_file("ch03.exs", __DIR__)
-
-defmodule EmployeeTest do
-  use ExUnit.Case
-
-  test "employee creation with default values" do
-    employee = EmployeeFunc.new()
-    assert employee.firstname == ""
-    assert employee.lastname == ""
-    assert employee.id == 0
-  end
-
-  test "employee creation with custom values" do
-    employee = EmployeeFunc.new("Jane", "Doe", 2)
-    assert employee.firstname == "Jane"
-    assert employee.lastname == "Doe"
-    assert employee.id == 2
-  end
-end
