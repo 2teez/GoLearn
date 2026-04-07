9bdfaca9f1be7828e74d139e175c565bc991f0f3
timothy adigun <adigun.timothy@gmail.com>
2026-04-07 23:09:43 +0100

    for loop practice

--- learning_go/ch04/main.go
+++ learning_go/ch04/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
-	fmt.Println("Start Here...")
+	// for loop
+	intVals := make([]int, 0, 100)
+
+	for i := 0; i < 100; i++ {
+		intVals[i] = rand.Intn(100)
+	}
+	fmt.Println(intVals)
 }
