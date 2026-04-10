a30e23946d0706a8ba53a646a7b2c10e11d6f833
timothy adigun <adigun.timothy@gmail.com>
2026-04-10 12:50:57 +0100

    maked a an anonymous function that returns another function

--- learning_go/ch05/exer05/main.exs
+++ learning_go/ch05/exer05/main.exs
@@ -1,14 +1,10 @@
 defmodule MathFunc do
-  def prefixer(str) do
-    &(str <> " " <> &1)
-  end
-
   def divi(_a, b) when b == 0, do: {0, "division by zero"}
   def divi(a, b), do: {a / b, :ok}
 end
 
 defmodule Main do
-  import MathFunc, only: [prefixer: 1, divi: 2]
+  import MathFunc, only: [divi: 2]
 
   def run(args) do
     if length(args) != 1 do
@@ -21,7 +17,8 @@ defmodule Main do
     count = File.read!(filename) |> String.length()
     IO.puts("File #{filename} has #{count} characters.")
 
-    hello_prefix = prefixer("Hello")
+    prefixer = &fn str -> str <> " " <> &1 end
+    hello_prefix = prefixer.("Hello")
     hello_prefix.("Bob") |> IO.puts()
     hello_prefix.("Maria") |> IO.puts()
     divi(3, 0) |> IO.inspect()
