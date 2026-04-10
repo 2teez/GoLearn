df0f30628372fc393169c4fd0c6685acd57d2f7e
timothy adigun <adigun.timothy@gmail.com>
2026-04-10 15:15:34 +0100

    using pointer, slice and others

--- learning_go/ch06/main.go
+++ learning_go/ch06/main.go
@@ -16,6 +16,24 @@ func main() {
 
 	p2 := MakePersonPointer("Jane", "Doe", 25)
 	fmt.Println(p2)
+	///
+	langs := []string{"Go", "Python", "Java"}
+	fmt.Println("Before Any Functions: ", langs)
+	UpdateSlice(langs, "C++")
+	fmt.Println("After UpdateSlice: ", langs)
+	GrowSlice(langs, "Rust")
+	fmt.Println("After GrowSlice: ", langs)
+}
+
+func UpdateSlice(strs []string, str string) {
+	index := len(strs) - 1
+	strs[index] = str
+	fmt.Println("Inside UpdateSlice:", strs)
+}
+
+func GrowSlice(strs []string, str string) {
+	strs = append(strs, str)
+	fmt.Println("Inside GrowSlice:", strs)
 }
 
 func MakePerson(firstName, lastName string, age int) Person {
