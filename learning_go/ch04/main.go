752431aba7df3eba389dadd1ac3a5c9f228e7e42
timothy adigun <adigun.timothy@gmail.com>
2026-04-10 06:32:22 +0100

    practice switch in golang

--- learning_go/ch04/main.go
+++ learning_go/ch04/main.go
@@ -7,10 +7,22 @@ import (
 
 func main() {
 	// for loop
-	intVals := make([]int, 0, 100)
+	intVals := make([]int, 100)
 
 	for i := 0; i < 100; i++ {
 		intVals[i] = rand.Intn(100)
 	}
 	fmt.Println(intVals)
+	for _, val := range intVals {
+		switch {
+		case val%6 == 0:
+			fmt.Println(val, "Six")
+		case val%3 == 0:
+			fmt.Println("Three")
+		case val%2 == 0:
+			fmt.Println("Two")
+		default:
+			fmt.Println(val, "Never mind")
+		}
+	}
 }
