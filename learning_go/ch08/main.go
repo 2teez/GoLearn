fbaf0b42d108b9684ae7ee3b423d5ab811e3c2cf
timothy adigun <adigun.timothy@gmail.com>
2026-04-11 09:31:27 +0100

    doing linkedlist in golang

--- learning_go/ch08/main.go
+++ learning_go/ch08/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 type Integer interface {
@@ -17,6 +18,11 @@ type Printable interface {
 type MyInt int
 type MyFloat float64
 
+type linkedList[T any] struct {
+	val  T
+	next *linkedList[T]
+}
+
 func main() {
 	fmt.Println(double(34.5))
 	fmt.Println(double(0.054))
@@ -25,7 +31,53 @@ func main() {
 	var intValue MyInt = 3
 	printIt(intValue)
 	printIt(MyFloat(3.14))
+	//
+	// linked list
+	list := &linkedList[int]{val: 1}
+	list.Add(2)
+	list.Add(3)
+	list.Add(4)
+	list.Add(5)
+	fmt.Printf("%v\n", list)
+	list.insert(6, 2)
+	fmt.Printf("%v\n", list)
+	list.remove(2)
+	fmt.Printf("%v\n", list)
+}
 
+func (l *linkedList[T]) Add(val T) {
+	newNode := l
+	for newNode.next != nil {
+		newNode = newNode.next
+	}
+	newNode.next = &linkedList[T]{val: val}
+}
+
+func (l *linkedList[T]) insert(val T, index int) {
+	newNode := l
+	for i := 0; i < index-1; i++ {
+		newNode = newNode.next
+	}
+	newNode.next = &linkedList[T]{val: val, next: newNode.next}
+}
+
+func (l *linkedList[T]) remove(index int) {
+	newNode := l
+	for i := 0; i < index-1; i++ {
+		newNode = newNode.next
+	}
+	newNode.next = newNode.next.next
+}
+
+func (l *linkedList[T]) String() string {
+	newNode := l
+	result := strings.Builder{}
+	for newNode != nil {
+		fmt.Fprintf(&result, "%v -> ", newNode.val)
+		newNode = newNode.next
+	}
+	result.WriteString("nil")
+	return result.String()
 }
 
 func double[I Integer](i I) I {
