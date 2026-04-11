d072f8e6268e03489b4e177370c92956da38185b
timothy adigun <adigun.timothy@gmail.com>
2026-04-11 07:39:11 +0100

    implemented intreface with underlying type

--- learning_go/ch08/main.go
+++ learning_go/ch08/main.go
@@ -10,16 +10,36 @@ type Integer interface {
 }
 
 type Printable interface {
-	String() string
+	~int | ~float64
+	fmt.Stringer
 }
+
+type MyInt int
+type MyFloat float64
 
 func main() {
 	fmt.Println(double(34.5))
 	fmt.Println(double(0.054))
 	fmt.Println(double(3))
+	//
+	var intValue MyInt = 3
+	printIt(intValue)
+	printIt(MyFloat(3.14))
 
 }
 
 func double[I Integer](i I) I {
 	return i + i
+}
+
+func printIt[P Printable](p P) {
+	fmt.Println(p)
+}
+
+func (i MyInt) String() string {
+	return fmt.Sprintf("%v", int(i))
+}
+
+func (f MyFloat) String() string {
+	return fmt.Sprintf("%v", float64(f))
 }
