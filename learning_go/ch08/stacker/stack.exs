58aae2d14ed7c8c34c3e22fdad67f41e8af3e125
timothy adigun <adigun.timothy@gmail.com>
2026-04-11 13:27:21 +0100

    removed the dependency module stacker and used all the functions in the stack file itself

--- learning_go/ch08/stacker/stack.exs
+++ learning_go/ch08/stacker/stack.exs
@@ -24,4 +24,41 @@ defmodule Stack do
   """
   @spec new() :: t()
   def new(), do: %__MODULE__{}
+
+  @doc """
+  Pushes an item onto the stack.
+
+  ## Examples
+
+      iex> stack = Stack.new()
+      iex> stack = Stack.push(stack, 1)
+      iex> Stack.pop(stack)
+      {1, Stack.new()}
+  """
+
+  @spec push(Stack.t(), any()) :: Stack.t()
+  def push(%Stack{} = stack, item) do
+    %Stack{stack | data: [item | stack.data]}
+  end
+
+  @doc """
+  Pops an item from the stack.
+
+  ## Examples
+
+      iex> stack = Stack.new([1, 2, 3])
+      iex> {item, stack} = Stack.pop(stack)
+      iex> item
+      3
+      iex> stack
+      Stack.new([1, 2])
+  """
+  @spec pop(Stack.t()) :: {any(), Stack.t()}
+  def pop(%Stack{} = stack) do
+    {stack.data |> Enum.reverse() |> hd(), %Stack{stack | data: rest(stack.data)}}
+  end
+
+  @spec rest([any()]) :: [any()]
+  defp rest([]), do: []
+  defp rest(list), do: list |> Enum.reverse() |> tl() |> Enum.reverse()
 end
