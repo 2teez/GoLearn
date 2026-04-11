6301173f876023410bf4d79f23bcb21f9934d557
timothy adigun <adigun.timothy@gmail.com>
2026-04-11 09:40:06 +0100

    implemented stack in golang

--- learning_go/ch08/stacker/main.go
+++ learning_go/ch08/stacker/main.go
@@ -9,6 +9,42 @@ type Stack[T any] struct {
 func main() {
 	stack := &Stack[int]{}
 	fmt.Println(stack)
+	//
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	fmt.Println(stack)
+	//
+	item, ok := stack.Pop()
+	if ok {
+		fmt.Println(item)
+	}
+	item, ok = stack.Pop()
+	if ok {
+		fmt.Println(item)
+	}
+	item, ok = stack.Pop()
+	if ok {
+		fmt.Println(item)
+	}
+	item, ok = stack.Pop()
+	if !ok {
+		fmt.Println("Stack is empty")
+	}
+}
+
+func (s *Stack[T]) Push(item T) {
+	s.data = append(s.data, item)
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+	if len(s.data) == 0 {
+		var zero T
+		return zero, false
+	}
+	item := s.data[len(s.data)-1]
+	s.data = s.data[:len(s.data)-1]
+	return item, true
 }
 
 func (s *Stack[T]) String() string {
