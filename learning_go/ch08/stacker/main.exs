45646d90e1f3c425d9705eae0dfed64e186654c1
timothy adigun <adigun.timothy@gmail.com>
2026-04-11 09:55:23 +0100

    stack in exlixir

--- learning_go/ch08/stacker/main.exs
+++ learning_go/ch08/stacker/main.exs
@@ -1,5 +1,37 @@
 defmodule Stack do
-  defstruct items: []
+  defstruct data: []
 
-  @type t :: %__MODULE__{items: [any()]}
+  @type t :: %__MODULE__{data: [any()]}
 end
+
+defmodule Stacker do
+  def push(stack, item) do
+    %Stack{stack | data: [item | stack.data]}
+  end
+
+  def pop(stack) do
+    {item, new_stack} = {List.first(stack.data), List.last(stack.data)}
+    {item, %Stack{stack | data: new_stack}}
+  end
+end
+
+defimpl Inspect, for: Stack do
+  def inspect(stack, _opts) do
+    "Stack{data: #{inspect(stack.data)}}"
+  end
+end
+
+defmodule Main do
+  def run do
+    stack = %Stack{}
+    stack = Stacker.push(stack, 1)
+    stack = Stacker.push(stack, 2)
+    stack = Stacker.push(stack, 3)
+    IO.inspect(stack)
+    {item, stack} = Stacker.pop(stack)
+    IO.inspect(item)
+    IO.inspect(stack)
+  end
+end
+
+Main.run()
