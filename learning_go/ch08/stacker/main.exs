4f92ddc8144af0cc7c309a0ed6c989e6b782db73
timothy adigun <adigun.timothy@gmail.com>
2026-04-11 10:53:48 +0100

    using elixir

--- learning_go/ch08/stacker/main.exs
+++ learning_go/ch08/stacker/main.exs
@@ -5,16 +5,32 @@ defmodule Stack do
 end
 
 defmodule Stacker do
-  def push(stack, item) do
+  @spec push(Stack.t(), any()) :: Stack.t()
+  def push(%Stack{} = stack, item) do
     %Stack{stack | data: [item | stack.data]}
   end
 
-  def pop(stack) do
-    {item, new_stack} = {List.first(stack.data), List.last(stack.data)}
-    {item, %Stack{stack | data: new_stack}}
+  @spec pop(Stack.t()) :: {any(), Stack.t()}
+  def pop(%Stack{} = stack) do
+    {stack.data |> Enum.reverse() |> hd(), %Stack{stack | data: rest(stack.data)}}
   end
+
+  @doc """
+  Returns the rest of the list after the first element.
+
+  ## Examples
+
+      iex> rest([1, 2, 3])
+      [2, 3]
+      iex> rest([1])
+      []
+  """
+  @spec rest([any()]) :: [any()]
+  defp rest([]), do: []
+  defp rest(list), do: list |> Enum.reverse() |> tl() |> Enum.reverse()
 end
 
+@spec inspect(Stack.t(), any()) :: String.t()
 defimpl Inspect, for: Stack do
   def inspect(stack, _opts) do
     "Stack{data: #{inspect(stack.data)}}"
@@ -27,6 +43,12 @@ defmodule Main do
     stack = Stacker.push(stack, 1)
     stack = Stacker.push(stack, 2)
     stack = Stacker.push(stack, 3)
+    IO.inspect(stack)
+    {item, stack} = Stacker.pop(stack)
+    IO.inspect(item)
+    IO.inspect(stack)
+    {item, stack} = Stacker.pop(stack)
+    IO.inspect(item)
     IO.inspect(stack)
     {item, stack} = Stacker.pop(stack)
     IO.inspect(item)
