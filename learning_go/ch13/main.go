b769e617493c20767238e02bff7ff4cf75b62700
timothy adigun <adigun.timothy@gmail.com>
2026-04-12 09:47:04 +0100

    main file adjusted

--- learning_go/ch13/main.go
+++ learning_go/ch13/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"ch13/reader"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Start Here...")
+	args := os.Args
+	if len(args) < 2 {
+		log.Println("./args[0] <filename>")
+		os.Exit(1)
+	}
+	file, err := reader.File(args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	bytes, err := reader.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(bytes))
 }
