021b82be6a43b936b0491e0f59cf113e0995190b
timothy adigun <adigun.timothy@gmail.com>
2026-04-16 08:03:59 +0100

    main file works

--- learning_go/junkz/practice_fs/main.go
+++ learning_go/junkz/practice_fs/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"practice_fs/files"
 	"practice_fs/flags"
 )
 
@@ -16,4 +18,25 @@ func main() {
 	}
 	fmt.Printf("Name: %s, Age: %d, Married: %t\n", *name, *age, *married_status)
 
+	// using the file package
+	writer, close, err := files.Create("temp_file.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
+
+	data := struct {
+		Name          string `json:"name"`
+		Age           int    `json:"age"`
+		MarriedStatus bool   `json:"married_status"`
+	}{
+		Name:          *name,
+		Age:           *age,
+		MarriedStatus: *married_status,
+	}
+	json, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	writer.Write(json)
 }
