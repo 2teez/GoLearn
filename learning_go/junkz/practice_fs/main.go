45cfd50a1561ed928eaefb7d4ff53a2119e5c3e5
timothy adigun <adigun.timothy@gmail.com>
2026-04-16 07:29:58 +0100

    main file

--- learning_go/junkz/practice_fs/main.go
+++ learning_go/junkz/practice_fs/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"practice_fs/flags"
 )
 
 func main() {
-	fmt.Println("Start Here...")
+	age, name, married_status := flags.Runs()
+	if *name == "" {
+		log.Println("Name is required")
+		flag.PrintDefaults()
+		return
+	}
+	fmt.Printf("Name: %s, Age: %d, Married: %t\n", *name, *age, *married_status)
+
 }
