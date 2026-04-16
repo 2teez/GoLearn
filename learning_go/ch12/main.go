da7b67fad2a34e032157709e1fa4da80ee528a65
timothy adigun <adigun.timothy@gmail.com>
2026-04-17 00:01:33 +0100

    using time.Sleep to see all the result of the go funcs

--- learning_go/ch12/main.go
+++ learning_go/ch12/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	r "ch12/routine"
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Start Here...")
+
+	r.Routine()
+	oneToTen := r.IterationParams{Start: 1, Stop: 10, Step: 1}
+	oneToHundred := r.IterationParams{Start: 1, Stop: 100, Step: 1}
+
+	var sumResult1, sumResult2 int
+	go func() {
+		sumResult1 = r.Summation(oneToTen)
+	}()
+	go func() {
+		sumResult2 = r.Summation(oneToHundred)
+	}()
+	time.Sleep(time.Second * 1)
+	fmt.Println(sumResult1, sumResult2)
 }
--- learning_go/ch12/routine/routine.go
+++ learning_go/ch12/routine/routine.go
@@ -2,11 +2,27 @@ package routine
 
 import (
 	"fmt"
+	"time"
 )
+
+type IterationParams struct {
+	Start int
+	Stop  int
+	Step  int
+}
+
+func Summation(params IterationParams) int {
+	sum := 0
+	for i := params.Start; i <= params.Stop; i += params.Step {
+		sum += i
+	}
+	return sum
+}
 
 func Routine() {
 	fmt.Println("Start")
 	go hello()
+	time.Sleep(time.Second)
 	fmt.Println("End")
 }
 
