4e4421c9bde439d2f984c4d5ff4e27a7bbaad405
timothy adigun <adigun.timothy@gmail.com>
2026-04-16 07:49:06 +0100

    package to create and write out to files

---
+++ learning_go/junkz/practice_fs/files/files.go
@@ -0,0 +1,15 @@
+package main
+
+import "os"
+
+func Create(filename string) (*os.File, func(), error) {
+	file, err := os.Create(filename)
+	if err != nil {
+		return nil, nil, err
+	}
+	return file, func() { file.Close() }, nil
+}
+
+func Write(file *os.File, data []byte) (int, error) {
+	return file.Write(data)
+}
