346eedb17cd5d7a5e258522e90baac18299868fa
timothy adigun <adigun.timothy@gmail.com>
2026-04-17 00:12:30 +0100

    goroutine learning

--- learning_go/ch12/main.go
+++ learning_go/ch12/main.go
@@ -12,13 +12,15 @@ func main() {
 	oneToTen := r.IterationParams{Start: 1, Stop: 10, Step: 1}
 	oneToHundred := r.IterationParams{Start: 1, Stop: 100, Step: 1}
 
-	var sumResult1, sumResult2 int
-	go func() {
-		sumResult1 = r.Summation(oneToTen)
-	}()
-	go func() {
-		sumResult2 = r.Summation(oneToHundred)
-	}()
-	time.Sleep(time.Second * 1)
-	fmt.Println(sumResult1, sumResult2)
+	{ // using goroutines with time.Sleep
+		var sumResult1, sumResult2 int
+		go func() {
+			sumResult1 = r.Summation(oneToTen)
+		}()
+		go func() {
+			sumResult2 = r.Summation(oneToHundred)
+		}()
+		time.Sleep(time.Second * 1)
+		fmt.Println(sumResult1, sumResult2)
+	}
 }
