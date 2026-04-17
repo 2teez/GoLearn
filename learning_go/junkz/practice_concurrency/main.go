de1fcb3fadc32ae3828815c0fb144bd31b7152c4
timothy adigun <adigun.timothy@gmail.com>
2026-04-17 14:49:32 +0100

    another waitgroup usage

--- learning_go/junkz/practice_concurrency/main.go
+++ learning_go/junkz/practice_concurrency/main.go
@@ -7,13 +7,29 @@ import (
 
 func main() {
 
-	var wg sync.WaitGroup
-	wg.Add(3)
-	for _, salutation := range []string{"hello", "greetings", "good day"} {
-		go func() {
-			defer wg.Done()
-			fmt.Println(salutation)
-		}()
+	{ // anonymous function goroutine
+		// without parameters
+		var wg sync.WaitGroup
+		wg.Add(3)
+		for _, salutation := range []string{"hello", "greetings", "good day"} {
+			go func() {
+				defer wg.Done()
+				fmt.Println(salutation)
+			}()
+		}
+		wg.Wait()
 	}
-	wg.Wait()
+
+	{ // anonymous function goroutine
+		// with parameters
+		var wg sync.WaitGroup
+		wg.Add(1)
+		for _, salutation := range []string{"hello", "greetings", "good day"} {
+			go func(salutation string) {
+				defer wg.Done()
+				fmt.Println(salutation)
+			}(salutation)
+		}
+		wg.Wait()
+	}
 }
