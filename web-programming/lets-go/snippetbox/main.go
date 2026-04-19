70d1fad2a7956cf27d8d0e487819ea7182ea11c5
timothy adigun <adigun.timothy@gmail.com>
2026-04-19 15:44:03 +0100

    added a mutex to the server in go

--- web-programming/lets-go/snippetbox/main.go
+++ web-programming/lets-go/snippetbox/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"log"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	server := &http.Server {
+		Addr: "127.0.0.1:4000",
+		Handler: mux,
+	}
+
+	log.Println("Starting server on :4000")
+	server.ListenAndServe()
 }
