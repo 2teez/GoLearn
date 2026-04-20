f342ffd8b9a86ffb48ec1decc9d99258c4851c7c
timothy adigun <adigun.timothy@gmail.com>
2026-04-20 11:46:35 +0100

    change the server to use multipliers

--- web-programming/lets-go/junkz/practice-server/main.go
+++ web-programming/lets-go/junkz/practice-server/main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
-
 func hello(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello, World!\n")
+	writer.Write([]byte("Hello, World!\n"))
 }
 
 func main() {
-	server := http.Server {
-		Addr: "127.0.0.1:8080",
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+
+	server := &http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
 	}
 
-	http.HandleFunc("/", hello)
 	server.ListenAndServe()
 }
