f1f1fc99ce99b3c9d4e1a870067ee0e93a8ea8d9
timothy adigun <adigun.timothy@gmail.com>
2026-04-21 10:44:25 +0100

    parsing an html file in golang

--- web-programming/lets-go/junkz/practice-server/ch02/snippetbox/cmd/web/handlers.go
+++ web-programming/lets-go/junkz/practice-server/ch02/snippetbox/cmd/web/handlers.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
