9d67649aecbb7c2b93d8277a74691bb541572571
timothy adigun <adigun.timothy@gmail.com>
2026-04-26 01:44:58 +0100

    moved the main.go file to the web folder

---
+++ web-programming/with_go/staticweb/cmd/web/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	file_server := http.FileServer(http.Dir("/public/"))
+	mux.Handle("/public/", http.StripPrefix("/public", file_server))
+
+	mux.HandleFunc("/", home)
+
+	server := http.Server{
+		Addr:    "127.0.0.1:4100",
+		Handler: mux,
+	}
+
+	log.Println("Server is running on http://127.0.0.1:4100")
+
+	server.ListenAndServe()
+}
