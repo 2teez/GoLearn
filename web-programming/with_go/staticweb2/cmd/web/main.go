95e4127294041a773d2d9debe3af056afafd1906
timothy adigun <adigun.timothy@gmail.com>
2026-04-26 03:11:55 +0100

    server working...

--- web-programming/with_go/staticweb2/cmd/web/main.go
+++ web-programming/with_go/staticweb2/cmd/web/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Start Here...")
+	mux := http.NewServeMux()
+
+	// using file server
+	file_server := http.FileServer(http.Dir("public"))
+	mux.Handle("/public/", http.StripPrefix("/public/", file_server))
+
+	server := &http.Server{
+		Addr:    "127.0.0.1:4200",
+		Handler: mux,
+	}
+	mux.HandleFunc("/", home)
+
+	log.Println("Started Server on: http://127.0.0.1:4200")
+	server.ListenAndServe()
 }
