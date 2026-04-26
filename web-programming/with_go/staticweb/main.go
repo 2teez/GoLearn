ec84a66365d1211390c9683ea1c6102ded7fb02b
timothy adigun <adigun.timothy@gmail.com>
2026-04-26 01:23:18 +0100

    web server in golang

--- web-programming/with_go/staticweb/main.go
+++ web-programming/with_go/staticweb/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
+func fs(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello, fs..."))
+}
+
 func main() {
-	fmt.Println("Start Here...")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", fs)
+
+	file_server := http.FileServer(http.Dir("/public/"))
+	mux.Handle("/public/", http.StripPrefix("/public", file_server))
+
+	server := http.Server{
+		Addr:    "127.0.0.1:4100",
+		Handler: mux,
+	}
+
+	log.Println("Server is running on http://127.0.0.1:4100")
+
+	server.ListenAndServe()
 }
